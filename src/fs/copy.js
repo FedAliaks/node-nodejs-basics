0fb4a29ab51afac56e248a369255ced278ffe3a9
Aliaksandr Fedukovich <1632637@mail.ru>
2024-01-26 13:53:37 +0300

    feat: add task for copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,40 @@
+import path from 'path';
+import fs from 'fs';
+import { constants } from 'fs';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+
 const copy = async () => {
-    // Write your code here 
+    // Write your code here
+    const pathToGoalDir = path.resolve(__dirname, 'files_copy');
+    const pathToSourceDir = path.resolve(__dirname, 'files');
+
+
+    fs.stat(pathToGoalDir, (err) => {
+        if (!err) {
+            throw 'FS operation failed';
+        } else {
+            fs.mkdir(pathToGoalDir, (err) => {
+                if (err) throw err;
+            })
+
+            fs.readdir(pathToSourceDir, (err, files) => {
+                if (err) throw err;
+                files.forEach((item)=> {
+                    fs.copyFile(path.resolve(pathToSourceDir, item), path.resolve(pathToGoalDir, item), (err) => {
+                        if (err) throw err;
+                    });
+                })
+            });
+        }
+    })
+
+
+
+
 };
 
 await copy();
