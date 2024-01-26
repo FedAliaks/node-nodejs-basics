a943d25f55823caefb348f076710faf6636b1f8b
Aliaksandr Fedukovich <1632637@mail.ru>
2024-01-26 15:31:46 +0300

    Feat: add solution ffor args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
     // Write your code here 
+    console.log(process.argv);
+    const  arrArguments = process.argv;
+    arrArguments.forEach((item, index) => {
+        if (item.startsWith('--')) {
+            console.log(`${item.slice(2)}: ${arrArguments[index + 1]}`)
+        }
+    })
 };
 
 parseArgs();
