657d6b8d0d2587b876fd30d5bed5919b27c011e8
Aliaksandr Fedukovich <1632637@mail.ru>
2024-01-26 15:19:15 +0300

    feat: add solution for env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
     // Write your code here 
+    const envVarObj = process.env;
+    for (let key in envVarObj) {
+        if (key.startsWith('RSS_')) {
+            console.log(`${key}=${envVarObj[key]}`);
+        }
+    }
 };
 
 parseEnv();
