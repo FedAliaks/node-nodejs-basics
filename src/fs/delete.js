f897f7d14c9255d91103d3b548c348a21f10f1e7
Aliaksandr Fedukovich <1632637@mail.ru>
2024-01-26 14:49:56 +0300

    ffeat: add solution for task delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,27 @@
+import path from 'path';
+import fs from 'fs';
+import { constants } from 'fs';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const remove = async () => {
     // Write your code here 
+
+    const pathToFile = path.resolve(__dirname, 'files', 'fileToRemove.txt');
+
+    fs.stat(pathToFile, (err) => {
+        if (err) {
+            throw 'FS operation failed';
+        } else {
+            fs.unlink(pathToFile, (err) => {
+                if (err) {
+                    throw 'FS operation failed';
+                }
+            })
+        }
+    })
 };
 
 await remove();
