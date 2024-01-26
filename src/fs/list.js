c13749c051d324ab31e344c8d84be220e6d9492e
Aliaksandr Fedukovich <1632637@mail.ru>
2024-01-26 14:57:53 +0300

    Feat: add solution for list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,27 @@
+import path from 'path';
+import fs from 'fs';
+import { constants } from 'fs';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
     // Write your code here 
+
+    const pathToFolder = path.resolve(__dirname, 'files');
+
+    fs.readdir(pathToFolder, (err, filesArray) => {
+        if (err || !filesArray.length ) {
+            throw 'FS operation failed';
+        } else {
+            filesArray.forEach((item) => console.log(item));
+        }
+    })
+
+
+
+
+
 };
 
 await list();
