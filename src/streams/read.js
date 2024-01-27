c3cbf02663bd0b638a665f57ba1731dc361d278a
Aliaksandr Fedukovich <1632637@mail.ru>
2024-01-27 15:56:52 +0300

    feat: add solution for read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import fs from 'fs';
+import { constants } from 'fs';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const read = async () => {
+
     // Write your code here 
+
+    const pathToFile = path.resolve(__dirname, 'files', 'fileToRead.txt');
+
+    const stream = fs.createReadStream(pathToFile);
 };
 
 await read();
