b8c3eee9d393814bbc15e30053f6dfd1eab3986d
Aliaksandr Fedukovich <1632637@mail.ru>
2024-01-28 17:05:06 +0300

    feat: add solution for stream transfform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { Transform } from 'stream';
+import { EOL } from 'os';
+
+
+
 const transform = async () => {
-    // Write your code here 
+    // Write your code here
+
+    const reverseText = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, `${chunk.toString().split('').reverse().join('')}${EOL}`);
+        }
+    })
+
+    process.stdin.pipe(reverseText).pipe(process.stdout);
 };
 
 await transform();
