a1f7f09ce91cc368e00e8d6d9a8940b5193c58e5
Aliaksandr Fedukovich <1632637@mail.ru>
2024-01-28 16:23:20 +0300

    Feat: add solution for stream write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import path from 'path';
+import fs, { WriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
     // Write your code here 
+    const pathToFile = path.resolve(__dirname, 'files', 'fileToWrite.txt');
+    const writeStream = new WriteStream(pathToFile);
+    process.stdin.on('data', (chunk) => {
+        const data = chunk.toString();
+        writeStream.write(chunk.toString());
+    })
 };
 
 await write();
