7f3a5beba09de86a5a5b10fe1a5b00832bdf3314
Aliaksandr Fedukovich <1632637@mail.ru>
2024-01-28 18:15:20 +0300

    feat: add solution for compress and decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import fs from "fs";
+import zlib from 'zlib';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
     // Write your code here 
+    const bigFile = fs.createReadStream(path.resolve(__dirname, "files", 'fileToCompress.txt'));
+    const smallFile = fs.createWriteStream(path.resolve(__dirname, "files", 'archive.gz'));
+    bigFile.pipe(zlib.createGzip()).pipe(smallFile);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,32 @@
+import fs from "fs";
+import zlib from 'zlib';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const decompress = async () => {
     // Write your code here 
+    const decompressFile = fs.createWriteStream(path.resolve(__dirname, "files", 'decompressFile.txt'));
+    const smallFile = fs.createReadStream(path.resolve(__dirname, "files", 'archive.gz'));
+
+    const chunksArr = [];
+
+    smallFile.on('data', (chunk) => {
+        chunksArr.push(chunk);
+    })
+
+    smallFile.on('end', () => {
+        const buffer = Buffer.concat(chunksArr);
+        zlib.unzip(buffer, (err, result) => {
+            if (err) {
+                console.log('error');
+            }
+            
+            decompressFile.write(result);
+        })
+    })
 };
 
 await decompress();
