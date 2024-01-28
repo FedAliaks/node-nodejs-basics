d48a67f729b10502278b2192245646dbd3a94b86
Aliaksandr Fedukovich <1632637@mail.ru>
2024-01-28 16:05:44 +0300

    Feat: add solution for straem read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,18 +1,17 @@
 import path from 'path';
 import fs from 'fs';
-import { constants } from 'fs';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
 const read = async () => {
-
-    // Write your code here 
-
+    // Write your code here
     const pathToFile = path.resolve(__dirname, 'files', 'fileToRead.txt');
-
     const stream = fs.createReadStream(pathToFile);
+    stream.on('data', (chunk) =>{
+        process.stdout.write(chunk);
+    });
 };
 
 await read();
