28aeb1f56027b37e825c756f2d4b1f7b31e400fe
Aliaksandr Fedukovich <1632637@mail.ru>
2024-01-29 13:03:06 +0300

    Feat: add solution for child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,23 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { spawn } from 'node:child_process';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+    const pathToFile = path.resolve(__dirname, 'files', 'script.js')
+
+    const childProcess = spawn('node', [pathToFile, ...args.split(' ')]);
+
+    process.stdin.on('data', (data) => {
+        childProcess.stdin.write(data);
+    });
+
+    childProcess.stdout.on('data', (chunk) => {
+        console.log(chunk.toString());
+    })
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess('--args1 --args2 --args3');
